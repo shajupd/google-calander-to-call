d1ce823f419c4445a9e9d3649c9512ba2fba12b8
shajupd <shajupd@webdura.tech>
2025-05-22 03:20:47 +0530

    Refactor TwilioService to accept phone number as parameter and update sync logic

--- src/app/api/sync/route.ts
+++ src/app/api/sync/route.ts
@@ -3,7 +3,6 @@ import { TwilioService } from "@/lib/twilio.service";
 import UserModel from "@/models/user.model";
 import { GoogleCalendarUtils } from "@/utils.ts/google-calendar.utils";
 
-
 export async function GET() {
   // This is a placeholder for the actual sync logic
   // You can implement your sync logic here
@@ -13,7 +12,7 @@ export async function GET() {
   const usersWithPhoneNumbers = users.filter((user) => user.phone);
 
   if (usersWithPhoneNumbers.length > 0) {
-     Promise.all(
+    Promise.all(
       usersWithPhoneNumbers.map(async (user) => {
         const phoneNumber = user.phoneNumber;
         const email = user.email;
@@ -21,9 +20,9 @@ export async function GET() {
         const refreshToken = user.refreshToken;
 
         const events = await GoogleCalendarUtils.getEvents(accessToken, refreshToken);
-        if(events?.length){
-        console.log(`Syncing phone number ${phoneNumber} for user ${email}`);
-        await TwilioService.createCall();
+        if (events?.length) {
+          console.log(`Syncing phone number ${phoneNumber} for user ${email}`);
+          await TwilioService.createCall(phoneNumber);
         }
         // Assuming you have a function to sync the phone number
         // await syncPhoneNumber(email, phoneNumber);
--- src/lib/twilio.service.ts
+++ src/lib/twilio.service.ts
@@ -5,10 +5,10 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 export class TwilioService {
-  static async createCall() {
+  static async createCall(phoneNumber: string) {
     const call = await client.calls.create({
       from: process.env.TWILIO_PHONE_NUMBER as string,
-      to: "+919074810177",
+      to: phoneNumber,
       url: "http://demo.twilio.com/docs/voice.xml",
     });
 
