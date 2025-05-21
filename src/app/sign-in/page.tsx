247756337ffe91b173e4a15a7b1e91f80a664f25
shajupd <shajupd@webdura.tech>
2025-05-22 04:35:01 +0530

    Remove session check and redirect from sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,14 +1,7 @@
-import { auth } from "@/auth/options";
 import GoogleSignInButton from "@/components/GoogleSignInButton/Index";
-import { redirect } from "next/navigation";
 import React from "react";
 
 const page = async () => {
-  const session = await auth();
-
-  if (!session) {
-    redirect("/in");
-  }
   return (
     <div className="w-screen h-screen flex flex-col gap-6 items-center justify-center">
       <GoogleSignInButton />
